f131fb2aff5a85f0004b65894e4386ae804a238d
thinhnq-ibl <nqt909@gmail.com>
2026-03-21 15:39:52 +0700

    update pipline

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -8,42 +8,46 @@ set -e
 # ========================================================
 
 echo "========================================="
-echo " STEP 1: PROCESSING SHARE_DATA "
+echo " STEP 1: PROCESSING CHECK-DATA-DISTRIBUTION "
 echo "========================================="
-cd ./share_data
+cd ./check-data-distribution
 source ../.venv/bin/activate
-python 1-get-cell-by-lat-lon.py
-python 2_find-cell-in-subzone.py
-python 3-get-pois-by-id.py
-python 4-get-district-for-cell.py
-python 5_sum_count.py
-python 6-get-probability-cell.py
+python compare_distribution.py
+python map_subzone.py
 
 echo "========================================="
-echo " STEP 2: PROCESSING GEN-CELL-FLOW-FOR-TEST "
+echo " STEP 2: PROCESSING SUBZONE-CELL "
+echo "========================================="
+cd ../subzone-cell
+source ../.venv/bin/activate
+python 1-get-cell-by-lat-lon.py
+python 2-get-pois-by-id.py
+python 3-get-district-to-cell.py
+python 4-get-prob-cell.py
+python 5-sum-pair.py
+
+echo "========================================="
+echo " STEP 3: PROCESSING GEN-CELL-FLOW-FOR-TEST "
 echo "========================================="
 cd ../gen-cell-flow-for-test
 source ../.venv/bin/activate
-python 1-find-cell-in-subzone.py
-python 2-get-cpc-cell-out.py
+python 1-get-cpc-cell-out.py
+python 2-get-cell-out.py
 python 3-only-get-percent.py
 
 echo "========================================="
-echo " STEP 3: PROCESSING GEN-OD-FLOW "
+echo " STEP 4: PROCESSING GEN-OD-FLOW "
 echo "========================================="
 cd ../gen-OD-flow
 source ../.venv/bin/activate
-python 1_sum_count_out.py
-python 2-find-cell-out-subzone.py
-python 3_share_out_to_cell.py
-python 4_sum_each_cell.py
-python 5-get-near-cell.py
-python 6-calculate-prob-in-per-cell-have-distance.py
-python 7-calculate-amount-in-per-cell-have-distance.py
-python 8-only-get-percent.py
+python 1-get-out-cell.py
+python 2-get-near-cell.py
+python 3-calculate-prob-in-per-cell-have-distance.py
+python 4-calculate-amount-in-per-cell-have-distance.py
+python 5-only-get-percent.py
 
 echo "========================================="
-echo " STEP 4: CALCULATING METRICS "
+echo " STEP 5: CALCULATING METRICS "
 echo "========================================="
 cd ../metric
 source ../.venv/bin/activate
